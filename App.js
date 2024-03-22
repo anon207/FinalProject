0f8db562a6b55603eb25ba2fe6f603428a5a7c69
anon207 <granbyroll207@gmail.com>
2024-03-21 21:03:32 -0400

    Added Calendar component and cell component

--- App.js
+++ App.js
@@ -1,10 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
+
+function Cell() {
+return(
+  <View style={styles.cell}/>
+);
+
+}
+
+function Calendar() {
+const calendarCells = [];
+for (let i = 1; i <= 42; i++) {
+  calendarCells.push(i);
+}
+  return(
+    <ScrollView contentContainerStyle={styles.calendar}>  
+      {calendarCells.map((index) => (
+        <Cell 
+        key={index}
+        />
+      ))}  
+    </ScrollView>
+  );
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Calendar/>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +40,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  calendar: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
+  cell:{
+    height: 100,
+    width: 54.02,
+    backgroundColor: 'white',
+    borderWidth: 1,
+    borderColor: 'gray',
+  }
 });
